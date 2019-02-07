d195c8ee888d7f98bbac4930496a626929b144e1
redsun1988 <silentiym>
2019-02-07 21:31:33 +0300

    QuickSort is implemented

--- ClassLibrary1/SortAlgorithms/QuickSort.cs
+++ ClassLibrary1/SortAlgorithms/QuickSort.cs
@@ -8,15 +8,40 @@ namespace BasicAlgorithms
     {
         public override List<int> Sort(List<int> sequence)
         {
-            int startIndex = 0;
-            int endIndex = sequence.Count - 1;
-            int pivotIndex = GetPivotIndexQuickSort(startIndex, endIndex);
-
-            throw new NotImplementedException();
+            int start = 0;
+            int end = sequence.Count - 1;
+            LandSequence(sequence, start, end);
+            return sequence;
+            //throw new NotImplementedException();
         }
+
+        private void LandSequence(List<int> sequence, int start, int end)
+        {
+            int pivotIndex = GetPivotIndexQuickSort(start, end);
+            int pivotValue = sequence[pivotIndex];
+            int l = start;
+            int r = end;
+            do
+            {
+                while (sequence[l] < pivotValue) { l++; }
+                while (sequence[r] > pivotValue) { r--; }
+
+                if (l <= r)
+                {
+                    Swap(sequence, l, r);
+                    l++;
+                    r--;
+                }
+            } while (l <= r);
+
+            if (start < r) LandSequence(sequence, start, r);
+
+            if (end > l) LandSequence(sequence, l, end);
+        }
+
         private static int GetPivotIndexQuickSort(int startIndex, int endIndex)
         {
-            return (endIndex - startIndex) / 2;
+            return startIndex + (endIndex - startIndex) / 2;
         }
     }
 }
